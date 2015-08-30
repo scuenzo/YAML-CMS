b9af1fe4130685a2611da2a261da1d4f01322a91
๖ۣۜSkyzohKey <SkyzohKeyx@users.noreply.github.com>
2015-08-30 02:06:37 +0200

    Updated comments

--- public/index.php
+++ public/index.php
@@ -7,18 +7,20 @@ define('app_path', base_path . DIRECTORY_SEPARATOR . 'app');
 define('content_path', base_path . DIRECTORY_SEPARATOR . 'content');
 define('layouts_path', base_path . DIRECTORY_SEPARATOR . 'layouts');
 
-// We start Slim
+// We start Slim and create a new PagesCollection
 $app = new \Slim\Slim(['debug' => true]);
 $pages = new \App\PagesCollection();
 
 // Let's load all the helpers funtions
 require app_path . DIRECTORY_SEPARATOR . 'helpers.php';
 
-// Let's create a rout for every YAML in the content directory
+// Let's create a route for every YAML file in the content directory
 foreach($pages as $page){
     // We use any cause we may need to access some page using POST to create forms for instance
     $app->any($page->getUrl(), function() use ($page, $app){
         echo $page->render();
     })->name($page->getName());
 }
+
+// Let's run our awesome app
 $app->run();
